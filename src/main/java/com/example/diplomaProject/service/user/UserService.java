d9d7cc040daacfa439970adafd86aa2c72f6f1dc
Nickolas2099 <iantoshka2003@mail.ru>
2023-12-07 13:08:41 +0300

    add userService

--- src/main/java/com/example/diplomaProject/service/user/UserService.java
+++ src/main/java/com/example/diplomaProject/service/user/UserService.java
@@ -1,2 +1,19 @@
-package com.example.diplomaProject.service.user;public interface UserService {
+package com.example.diplomaProject.service.user;
+
+import com.example.diplomaProject.domain.entity.User;
+import com.example.diplomaProject.domain.response.Response;
+import org.springframework.http.ResponseEntity;
+
+public interface UserService {
+
+    ResponseEntity<Response> getAll();
+
+    ResponseEntity<Response> getById(Long id);
+
+    ResponseEntity<Response> add(User user);
+
+    ResponseEntity<Response> update(User user, Long id);
+
+    ResponseEntity<Response> remove(Long id);
+
 }
--- src/main/java/com/example/diplomaProject/service/user/UserServiceImpl.java
+++ src/main/java/com/example/diplomaProject/service/user/UserServiceImpl.java
@@ -1,2 +1,95 @@
-package com.example.diplomaProject.service;public class UserService {
+package com.example.diplomaProject.service.user;
+
+import com.example.diplomaProject.domain.constant.Code;
+import com.example.diplomaProject.domain.entity.User;
+import com.example.diplomaProject.domain.response.Response;
+import com.example.diplomaProject.domain.response.SuccessResponse;
+import com.example.diplomaProject.domain.response.error.Error;
+import com.example.diplomaProject.domain.response.error.ErrorResponse;
+import com.example.diplomaProject.repository.UserRepository;
+import com.example.diplomaProject.util.ValidationUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+    private final ValidationUtils validation;
+
+
+    public ResponseEntity<Response> getAll() {
+
+        List<User> users = userRepository.findAllSortBySecondName();
+
+        return new ResponseEntity<>(SuccessResponse.builder().data(users).build(), HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<Response> getById(Long id) {
+
+        Optional<User> userOptional = userRepository.findById(id);
+        if(userOptional.isPresent()) {
+            return new ResponseEntity<>(SuccessResponse.builder().data(userOptional.get()).build(), HttpStatus.OK);
+        } else {
+            log.error("user with id: {} is not found", id);
+            return new ResponseEntity<>(ErrorResponse.builder()
+                    .error(Error.builder().code(Code.NOT_FOUND).message("Пользователь не найден").build())
+                    .build(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @Override
+    public ResponseEntity<Response> add(User user) {
+
+        validation.validationRequest(user);
+        userRepository.save(user);
+
+        return new ResponseEntity<>(SuccessResponse.builder().build(), HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<Response> update(User user, Long id) {
+
+        validation.validationRequest(user);
+
+        Optional<User> userOptional = userRepository.findById(id);
+        if(userOptional.isPresent()) {
+            User baseUser = userOptional.get();
+            baseUser.setFirstName(user.getFirstName());
+            baseUser.setSecondName(user.getSecondName());
+            baseUser.setPassword(user.getPassword());
+            baseUser.setRoles(user.getRoles());
+            userRepository.save(baseUser);
+
+            return new ResponseEntity<>(SuccessResponse.builder().build(), HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(ErrorResponse.builder()
+                .error(Error.builder().code(Code.INVALID_VALUE).message("Пользователь не найден").build())
+                .build(), HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    public ResponseEntity<Response> remove(Long id) {
+
+        if(!userRepository.existsById(id)) {
+            log.error("user with id: {} is not found", id);
+            return new ResponseEntity<>(ErrorResponse.builder()
+                    .error(Error.builder().code(Code.NOT_FOUND).message("Пользователь не найден").build())
+                    .build(), HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(SuccessResponse.builder().build(), HttpStatus.OK);
+    }
+
+
 }
