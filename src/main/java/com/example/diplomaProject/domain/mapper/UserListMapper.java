2654236e5f3b9d9fe36a6a01c3b0880efc186bc5
Nickolas2099 <iantoshka2003@mail.ru>
2023-12-14 10:31:50 +0300

    add userListMapper

---
+++ src/main/java/com/example/diplomaProject/domain/mapper/UserListMapper.java
@@ -0,0 +1,16 @@
+package com.example.diplomaProject.domain.mapper;
+
+import com.example.diplomaProject.domain.dto.UserDto;
+import com.example.diplomaProject.domain.entity.User;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = UserMapper.class)
+public interface UserListMapper {
+
+    List<UserDto> toDtoList(List<User> users);
+
+    List<User> toEntityList(List<UserDto> users);
+
+}
