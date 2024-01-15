0c52d8413c37d81da717de98f284cbee138a1c04
Nickolas2099 <iantoshka2003@mail.ru>
2024-01-15 12:00:37 +0300

    add expiredJwt exception handling

--- src/main/java/com/example/diplomaProject/domain/response/exception/ErrorHandler.java
+++ src/main/java/com/example/diplomaProject/domain/response/exception/ErrorHandler.java
@@ -3,6 +3,7 @@ package com.example.diplomaProject.domain.response.exception;
 import com.example.diplomaProject.domain.constant.Code;
 import com.example.diplomaProject.domain.response.error.Error;
 import com.example.diplomaProject.domain.response.error.ErrorResponse;
+import io.jsonwebtoken.ExpiredJwtException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,4 +31,10 @@ public class ErrorHandler {
                 .message("Внутренняя ошибка сервиса").build()).build(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<ErrorResponse> handleExpiredJwtException(ExpiredJwtException ex) {
+        log.error("json web token is already expired: {}", ex.toString());
+        return new ResponseEntity<>(ErrorResponse.builder().error(Error.builder()
+                .code(Code.FORBIDDEN).message("токен больше не действителен").build()).build(), HttpStatus.FORBIDDEN);
+    }
 }
