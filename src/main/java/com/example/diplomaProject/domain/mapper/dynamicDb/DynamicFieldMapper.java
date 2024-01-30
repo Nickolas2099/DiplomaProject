8460464ed1757e8ebdf57357498dc515b28fe1a5
Nickolas2099 <iantoshka2003@mail.ru>
2024-01-30 13:08:02 +0300

    add mappers for new entities

---
+++ src/main/java/com/example/diplomaProject/domain/mapper/dynamicDb/DynamicFieldMapper.java
@@ -0,0 +1,13 @@
+package com.example.diplomaProject.domain.mapper.dynamicDb;
+
+import com.example.diplomaProject.domain.dto.Field;
+import com.example.diplomaProject.domain.entity.DynamicField;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface DynamicFieldMapper {
+
+    DynamicField toEntity(Field field);
+
+    Field toDto(DynamicField field);
+}
